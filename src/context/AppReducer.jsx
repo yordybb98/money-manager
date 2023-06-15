eb3bf2f50156fed4d8ccd51317fde1c6e591e39d
Yordan Beltran <yordanbb98@gmail.com>
2023-06-15 10:44:23 -0400

    Adding Transactions

---
+++ src/context/AppReducer.jsx
@@ -0,0 +1,11 @@
+export default (state, action) => {
+    switch (action.type) {
+        case "ADD_TRANSACTION":
+            return {
+                ...state,
+                transactions: [...state.transactions, action.payload]
+            }
+        default:
+            return state
+    }
+}
